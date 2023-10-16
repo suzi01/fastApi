58e4dade3276b81a4ea07b703bd37a3daa7487cf
suzannah.thompson <suzannahthompson@hotmail.co.uk>
2023-10-16 08:38:48 +0100

    [FreecodeCamp - Additional changes for setup for git, adjust styling in main.py]

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ app = FastAPI()
 async def root():
     return {"message": "welcome to my api"}
 
+
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
 
 @app.post("/createposts")
 def create_posts(body: dict = Body(...)):
